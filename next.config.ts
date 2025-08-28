62ad669b1b8c6245218ac91cbd54e7f2e5083ffc
jarray-thamer <thamer99jarray@gmail.com>
2025-08-28 12:36:48 +0100

    disable eslint

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  rules: {
-    "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/no-require-imports": "off",
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
