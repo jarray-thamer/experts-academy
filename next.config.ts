d8c063bda9d1487bcb4b4a588f95433b4ed5906b
jarray-thamer <thamer99jarray@gmail.com>
2025-08-28 12:17:41 +0100

    eslint config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   rules: {
     "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-require-imports": "off",
   },
 };
 
