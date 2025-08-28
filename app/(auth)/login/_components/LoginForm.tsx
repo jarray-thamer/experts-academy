3f7ebd1da1497fa80e0e4aee1bb5cbbe61c159c8
jarray-thamer <thamer99jarray@gmail.com>
2025-08-28 12:08:45 +0100

    commenting no used imports

--- app/(auth)/login/_components/LoginForm.tsx
+++ app/(auth)/login/_components/LoginForm.tsx
@@ -7,8 +7,8 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+// import { Input } from "@/components/ui/input";
+// import { Label } from "@/components/ui/label";
 import { authClient } from "@/lib/auth-client";
 import { Loader2Icon } from "lucide-react";
 import { Google } from "developer-icons";
